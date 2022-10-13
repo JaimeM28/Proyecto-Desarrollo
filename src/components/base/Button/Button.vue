3cf458cade883fe895eb55c2f7a7aeb81d169899
JaimeM28 <mirandaserranojaimemanuelnuel@gmail.com>
2022-10-13 16:58:27 -0500

    feat: ✨  creando botones

---
+++ src/components/base/Button/Button.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+    text: {
+        type: String,
+        default: 'Button'
+    }
+})
+
+const button = `w-full h-full bg-zinc-800 hover:bg-zinc-700
+                px-3 text-white rounded leading-8` 
+
+</script>
+
+<template>
+    <button :class="button"> {{ text }}</button>
+</template>
+
+
+<style>
+
+</style>
---
+++ src/components/base/Button/Secondary.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+    text: {
+        type: String,
+        default: 'Button'
+    }
+})
+
+
+const button=`w-full h-full border-2 border-[transparent] 
+                px-3 leading-8 hover:font-semibold rounded
+                border-b-zinc-800 hover:border-b-zinc-700` 
+
+</script>
+
+<template>
+    <button :class="button"> {{ text }}</button>
+</template>
