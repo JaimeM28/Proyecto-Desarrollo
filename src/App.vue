6655045583628f13583f37cee525118086df5b1f
JaimeM28 <mirandaserranojaimemanuelnuel@gmail.com>
2022-10-13 20:13:07 -0500

    feat:  :sparkles: Creando Navbar

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Navbar from './components/container/Navbar.vue';
+
+</script>
+
+<template>
+<Navbar/>
+</template>
+
---
+++ src/components/container/Navbar.vue
@@ -0,0 +1,31 @@
+<script setup>
+import Secondary from '../base/Button/Secondary.vue';
+import Button from '../base/Button/Button.vue';
+
+</script>
+
+<template>
+    <div class="navbar fixed top-0 bg-yellow-300">
+    <div class="navbar__img">
+        <img src="../../assets/images/logo.png" alt="logo olla creativa">
+    </div>
+    <div class="navbar__href">
+        <a href="#">Inicio</a>
+        <a href="#">Sobre nosotros</a>
+        <a href="#">Contactanos</a>
+    </div>
+    <div class="navbar__btn">
+        <Secondary :text="'Registro'"/>
+        <Button :text="'Iniciar Sesion'"/>
+    </div>
+    </div>
+</template>
+
+<style>
+
+</style>
+
+
+
+
+
