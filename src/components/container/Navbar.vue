6cac6a34ae8403abf60d164ebda308be98f70248
JaimeM28 <mirandaserranojaimemanuelnuel@gmail.com>
2022-10-13 22:42:51 -0500

    feat: :lipstick: Navbar responsive

--- src/components/container/Navbar.vue
+++ src/components/container/Navbar.vue
@@ -1,13 +1,28 @@
-<script setup>
+<script>
 import Secondary from '../base/Button/Secondary.vue';
 import Button from '../base/Button/Button.vue';
-
+export default {
+    data() {
+        return {
+            showMobileMenu: false,
+        };
+    },
+    methods: {
+        showMenu() {
+            this.showMobileMenu = !this.showMobileMenu;
+        },
+    },
+    components: { Button, Secondary }
+};
 </script>
 
 <template>
-    <div class="navbar fixed top-0 bg-yellow-300">
+    <div class="navbar fixed top-0 bg-yellow-300 w-full">
+    <img src="../../assets/images/Hamburger.png" alt="menu" id="menu" @click="showMenu()">
+    <div class="navbar__content bg-yellow-300" :class="this.showMobileMenu ? 'open-menu' : 'closed-menu'">
     <div class="navbar__img">
         <img src="../../assets/images/logo.png" alt="logo olla creativa">
+        
     </div>
     <div class="navbar__href">
         <a href="#">Inicio</a>
@@ -19,9 +34,35 @@ import Button from '../base/Button/Button.vue';
         <Button :text="'Iniciar Sesion'"/>
     </div>
     </div>
+    </div>
 </template>
 
 <style>
+
+
+
+@media screen and (max-width: 900px) {
+ 
+
+  
+a{
+  width: 100%;  
+  display: flex;
+  justify-content: center;
+}
+    #menu:hover{
+        background-color:rgb(254 240 138);
+    }
+
+  
+
+    
+
+    .navbar__btn{
+        flex-direction: column;
+    }
+   
+}
 
 </style>
 
--- src/theme/navbar.container.js
+++ src/theme/navbar.container.js
@@ -2,12 +2,20 @@ const plugin = require('tailwindcss/plugin')
 
 module.exports = plugin(function({addComponents, theme}){
     addComponents({
-        '.navbar':{
+        'navbar':{
+            display: 'block',
+            height: '50px'
+        },
+        '#menu':{
+            height:'36px',
+            display: 'none'
+        },
+        '.navbar__content':{
             width: '100%',
             height: '50px',
             display:'grid',
             gridTemplateColumns:'repeat(3,1fr)',
-            gridGap:'10px',
+            gridGap:'20px',
 
             '.navbar__img':{
                 display: 'flex',
@@ -20,15 +28,12 @@ module.exports = plugin(function({addComponents, theme}){
             },
 
             '.navbar__href':{
-                display:'grid',
-                gridTemplateColumns:'repeat(3,1fr)',
-                gridGap:'10px',
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
                 'a':{
-                    display:'flex',
-                    justifyContent:'center',
-                    alignItems:'center',
                     '&:hover':{
-                        fontWeight:'800'
+                        fontWeight:'800',
                     }
             }
             },
@@ -43,12 +48,61 @@ module.exports = plugin(function({addComponents, theme}){
                 'Button':{
                     height:'35px',
                 }
-
             }
+        },
 
+        '@media screen and (max-width:900px)':{
+            '#menu':{
+                display: 'block',
+                position: 'relative',
+                top: '7px',
+                left: '10px',
+                'hover':{
+                    backgroundColor:'rgb(254 240 138)',
+                }
+            },
+
+            '.navbar':{
+                height: '50px'
+            },
+
+            '.open-menu' :{
+                opacity: '1',
+                height: '230px'
+                },
+
+            '.closed-menu': {
+                opacity: '0',
+                height: '0',
+                padding: '0',
+                },
+
+            '.navbar__content':{
+                display: 'flex',
+                gap: '7px',
+                justifyContent: 'space-between',
+                flexDirection: 'column',
+                zIndex: '100',
+                position: 'relative',
+                transition: 'all 0.2s ease-out',
+
+                '.navbar__href' :{
+                    flexDirection: 'column',
+                    gap: '10px',
+                    'a':{
+                        width: '100%', 
+                        display: 'flex',
+                        justifyContent: 'center'
+                      }
+                  },
+
+                  '.navbar__btn':{
+                    flexDirection: 'column'
+                }
+
+
+                }
         }
-
-        
 
     })
 })
